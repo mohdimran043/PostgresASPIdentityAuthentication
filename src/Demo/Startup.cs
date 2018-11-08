e02de89ba2d9ccacf3e803f6e3ef9b152b4d12c5
Mohd Imran <mohdimran043@gmail.com>
2018-11-09 01:25:45 +0300

    Add project files.

---
+++ src/Demo/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Demo.Models;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(Demo.Startup))]
+namespace Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            // CreateDatabaseIfNotExists is the default initializer if not specified
+            //Database.SetInitializer<ApplicationDbContext>(new CreateDatabaseIfNotExists<ApplicationDbContext>());
+
+            // NullDatabaseInitializer will turn off migration. 
+            // You need to manually create the data tables
+            Database.SetInitializer<ApplicationDbContext>(new NullDatabaseInitializer<ApplicationDbContext>());
+        }
+    }
+}
